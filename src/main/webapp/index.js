9de5e525eac4068a5f794673f6f9783efaca6efc
Thomas Klemmensen <klemmen@dk.ibm.com>
2017-01-09 09:31:43 -0600

    Try to convert JSON to HTML

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -7,7 +7,7 @@
 xhrGet("SimpleServlet", function(responseText){
 	// add to document
 	var mytitle = document.getElementById('message');
-	mytitle.innerHTML = responseText;
+	mytitle.innerHTML = parseJson(responseText);
 
 }, function(err){
 	console.log(err);
@@ -46,9 +46,9 @@ function xhrGet(url, callback, errback){
 	xhr.send();
 }
 
-/*function parseJson(str){
+function parseJson(str){
 	return window.JSON ? JSON.parse(str) : eval('(' + str + ')');
-}*/
+}
 /*function prettyJson(str){
 	// If browser does not have JSON utilities, just print the raw string value.
 	return window.JSON ? JSON.stringify(JSON.parse(str), null, '  ') : str;
